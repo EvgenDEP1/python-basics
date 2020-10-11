94921185abf65f894af83beb4680efb9ebf3a800
EvgenDEP1 <jen_dostovalov@mail.ru>
2020-10-11 15:22:31 +0500

    200914

--- 03.lists.py
+++ 03.lists.py
@@ -8,12 +8,12 @@ print(mock_student_marks)
 # print(mock_student_marks.index(5, 1))
 # print(mock_student_marks.index(5, 1, 5))
 
-print(mock_student_marks.count(5))
-print(mock_student_marks.count(2))
+# print(mock_student_marks.count(5))
+# print(mock_student_marks.count(2))
 
-if mock_student_marks.count(15):
-    print('15 index:', mock_student_marks.count(15))
-
-if mock_student_marks.count(5):
-    print('5 index:', mock_student_marks.count(5))
+num = 4 # abstraction concrete number > variable num
+if mock_student_marks.count(num):
+    print(num, 'found', mock_student_marks.count(num), 'times, first index:', mock_student_marks.index(num))
+else:
+    print(num, 'not found')
 
