18bc16f8a6121fbae1a21e4b30b3ad5ddc488a7d
EvgenDEP1 <jen_dostovalov@mail.ru>
2020-10-11 14:43:29 +0500

    200914

--- 02.lists.py
+++ 02.lists.py
@@ -3,21 +3,21 @@
 # while True:
 #     mark = input('Введите оценку студента:\n')
 #     if mark:
-#         student_marks.append(mark)
+#         student_marks.append(int(mark))
 #     else:
 #         break
 # print('Ввод завершен')
 # print(student_marks)
 
-mock_student_marks = ['5', '4', '3', '2', '5']  # wrong mock
-# mock_student_marks = [5, 4, 3, 2, 5]  # correct mock
+# mock_student_marks = ['5', '4', '3', '2', '5'] wrong mock
+mock_student_marks = [5, 4, 3, 2, 5]  # correct mock
 student_marks = mock_student_marks
 
 # data processing
 i = 0
 avg_mark = 0
 while i < len(student_marks):
-    avg_mark +=int(student_marks[i])
+    avg_mark += student_marks[i]
     i += 1
 avg_mark /= len(student_marks)
 print('средний балл', avg_mark)
