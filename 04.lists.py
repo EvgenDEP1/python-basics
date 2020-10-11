a119bda4c851bcffcbe5d24e2426fffe8aeb9018
EvgenDEP1 <jen_dostovalov@mail.ru>
2020-10-11 16:23:02 +0500

    200914

--- 04.lists.py
+++ 04.lists.py
@@ -7,7 +7,28 @@ lesson_dates = [
  ]
 student_marks = [5, 4, 3, 2, 5]
 
-i = 0
-while i < len(student_marks):
-    print(lesson_dates[i], 'оценка', student_marks[i])
-    i += 1
+# i = 0
+# while i < len(student_marks):
+#     print(lesson_dates[i], 'оценка', student_marks[i])
+#     i += 1
+
+# for item in student_marks:  # item
+#     print('оценка', item)
+#
+# for item in enumerate(student_marks):  # pairs -> (num, item)
+#     print('оценка', item)
+
+for item in enumerate(student_marks):  # pairs -> (num, item)
+    i, mark = item
+    print('оценка', item, 'или', i, mark)
+
+
+# for item in enumerate(student_marks):  # pairs -> (num, item)
+#     print('оценка', item)
+
+
+# feature
+# in place
+for i, mark in enumerate(student_marks):  # pairs -> (num, item)
+    print('оценка', i, mark)
+    # print(lesson_dates[i], 'оценка', mark)
