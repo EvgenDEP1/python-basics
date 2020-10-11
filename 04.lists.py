fa9e79e9c9bcd39c13e7b52447f9a778f1b5ffce
EvgenDEP1 <jen_dostovalov@mail.ru>
2020-10-11 15:32:13 +0500

    200914

---
+++ 04.lists.py
@@ -0,0 +1,13 @@
+lesson_dates = [
+    '14.09.2020',
+    '19.09.2020',
+    '21.09.2020',
+    '27.09.2020',
+    '28.09.2020'
+ ]
+student_marks = [5, 4, 3, 2, 5]
+
+i = 0
+while i < len(student_marks):
+    print(lesson_dates[i], 'оценка', student_marks[i])
+    i += 1
