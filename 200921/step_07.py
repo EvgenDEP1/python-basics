1e3cef5b1ad39fb664f020c8be994352e46096b5
EvgenDEP1 <jen_dostovalov@mail.ru>
2020-10-12 12:02:15 +0500

    200921

--- 200921/step_07.py
+++ 200921/step_07.py
@@ -1,11 +1,11 @@
 # class_pupils = input('Введите имена учеников через запятую:\n')
 
-class_pupils = 'Полина , Антон, Арсений , Евгений, Алексей, Тимур'
+class_pupils = 'Полина ,Антон, Арсений , Евгений, Алексей, Тимур'
 correct_result = ['Полина', 'Антон', 'Арсений', 'Евгений', 'Алексей', 'Тимур']
 
 # print('Ученики класса:', class_pupils)
 
-_result = class_pupils.split(', ')
+_result = class_pupils.split(',')
 result = []
 for item in _result:
     result.append(item.strip())
